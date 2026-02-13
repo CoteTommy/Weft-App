9ad4a0d237005b367a710de2e9c9176bfc1b9b77
ctommy <nordik.tommy@gmail.com>
2026-02-13 11:29:07 -0500

    feat: add eslint-plugin-simple-import-sort for improved import organization

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -18,6 +19,24 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000', '^react$', '^react-dom$', '^react-router-dom$', '^react-router$'],
+            ['^@?\\w'],
+            ['^@app/', '^@features/', '^@shared/', '^@lib/', '^@runtime/', '^@platform/'],
+            ['^\\.\\./', '^\\./'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'no-duplicate-imports': ['error', { allowSeparateTypeImports: false }],
     },
   },
 ])
